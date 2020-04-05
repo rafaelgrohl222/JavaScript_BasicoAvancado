b5ae62433f7eff5af32a3d96524f3b5b4788347b
Rafael Brigido <rafaelgrohl222@gmail.com>
2020-04-05 11:40:41 -0400

    Curso de JavaScript Moderno do básico ao avançado

--- Aula4_Js-variaveisComLet/index.js
+++ Aula4_Js-variaveisComLet/index.js
@@ -1,3 +1,5 @@
+//VARIAVEIS: let
+//NÃO PRECISA SER INICIALIZADA
 let nomeCliente2;
 nomeCliente2 = 'Qualquer valor de variável';//iniicializando a variável
 
@@ -10,3 +12,13 @@ console.log(nome, 'nasceu em 1984.');
 console.log('Em 2000' ,nome ,'conheceu Maria.');
 console.log('Maria teve 1 filho com' , nome , 'em 2015.');
 console.log('O filho de' , nome , 'se chama Eduardo.');
+
+//VARIAVEL CONSTANATE NÕA MUDA: const
+//TEMQUE SER INICIALIZADA SEMPRE
+const primeiroNumero = 5;//number
+const segundoNumero = 10;//number
+const result = primeiroNumero + segundoNumero;
+console.log('Resultado:', result);
+
+//QUAL É O TIPO DA VARIAVEL?
+console.log('TIPO DA VARIAVEL É:',typeof primeiroNumero);
