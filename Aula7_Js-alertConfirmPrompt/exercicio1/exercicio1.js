5a7ed445c7f3b8dc447bc7c21f6027f24beb4e3f
Rafael Brigido <rafaelgrohl222@gmail.com>
2020-04-11 15:14:13 -0400

    Curso de JavaScript Moderno do básico ao avançado

---
+++ Aula7_Js-alertConfirmPrompt/exercicio1/exercicio1.js
@@ -0,0 +1,6 @@
+let umaString = 'Rafael de Freitas';
+
+console.log('Nome:',umaString.toUpperCase());
+console.log('Nome 2:', umaString.toLowerCase());
+
+//Documentação: w3schools.com ou MDN Web Docs 
