646dedb3fa1133c56bebfd4e15edc70256ea8008
Rafael Brigido <rafaelgrohl222@gmail.com>
2020-04-13 12:38:05 -0400

    Curso de JavaScript Moderno do básico ao avançado

--- Aula8_Js-maisSobreNumeros/index.js
+++ Aula8_Js-maisSobreNumeros/index.js
@@ -24,12 +24,24 @@ console.log('Arredondamento Casas decimais:', num2.toFixed(2));//.toFixed(casasD
 let temp = num1 * 'ola';
 console.log('Não é um number?:', Number.isNaN(temp));//.isNaN(let)
 
-/*PRECISÃO DE NUMEROS C/ CASAS DECIMAIS- IEEE 754-2008*/
+/*IMPRECISÃO DE NUMEROS C/ CASAS DECIMAIS- IEEE 754-2008*/
 let num3 = 0.7;// Number
 let num4 = 0.1;// Number
 
 num3 += num4;// 0.8
 num3 += num4;// 0.9
 num3 += num4;// 1.0
+num3 += num4;// 1.1
 
-console.log('Resultado:', num3);//13min aula 21.
+/*Não foi Retirando a imprecisão numeral*/
+console.log('Resultado s/ imprecisão:', num3);
+
+/*------------*/
+console.log('');//Espaço
+/*Foi Retirando a imprecisão numeral*/
+//num3 = Number(num3.toFixed(2));/(2)são duas casas decimais. ->pode ser usado assim<-
+//parseFloat(); retorna um número de ponto flutuante
+num3 = parseFloat(num3.toFixed(2));
+console.log('Resultado c/ precisão:', num3);//12min aula 21.
+/*verivicando se existe casa decimaia "true ou false"*/
+console.log('Casas decimais ou Não?:', Number.isInteger(num3));//.isInteger(let)
