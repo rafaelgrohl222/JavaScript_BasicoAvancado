a8944ff1a9638c9494767da0260943f139c8b3ec
Rafael Brigido <rafaelgrohl222@gmail.com>
2020-05-07 14:53:54 -0400

    Curso de JavaScript Moderno do básico ao avançado (ES6)

--- Aula13_Js-aplicacaoFuncaoArrayObjetos/js/script.js
+++ Aula13_Js-aplicacaoFuncaoArrayObjetos/js/script.js
@@ -1,9 +1,18 @@
 function meuEscopo () {
     const form = document.querySelector('.form');
-    //espião
-    form.onsubmit = function (evento) {
-        alert('Eviado com "SUCESSO"!');
-    };
-}
+    
+    function recebeEventoForm (evento) {
+        evento.preventDefault();//previne contra envio de formulário
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        console.log("Nome:", nome.value,"Sobrenome:", sobrenome.value,"Peso:", peso.value,"Altura:", altura.value);
+    }
+    //adiciona um escutador de evento no formulário
+    form.addEventListener('submit', recebeEventoForm);
+}//Fim meuEscopo
 meuEscopo();
-//20min
+//28min
