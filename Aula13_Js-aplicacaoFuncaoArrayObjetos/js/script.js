79383ac3fba87e36e36850d9258d070e6d235628
Rafael Brigido <rafaelgrohl222@gmail.com>
2020-05-08 13:10:26 -0400

    Curso de JavaScript Moderno do básico ao avançado (ES6)

--- Aula13_Js-aplicacaoFuncaoArrayObjetos/js/script.js
+++ Aula13_Js-aplicacaoFuncaoArrayObjetos/js/script.js
@@ -1,5 +1,8 @@
 function meuEscopo () {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
     
     function recebeEventoForm (evento) {
         evento.preventDefault();//previne contra envio de formulário
@@ -9,7 +12,18 @@ function meuEscopo () {
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
 
-        console.log("Nome:", nome.value,"Sobrenome:", sobrenome.value,"Peso:", peso.value,"Altura:", altura.value);
+        //add 1 objetos p/ cada array pessoas
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>Nome: ${nome.value}, Sobrenome: ${sobrenome.value},` +
+        ` Peso: ${peso.value}Kg, Altura: ${altura.value}.</p>`
     }
     //adiciona um escutador de evento no formulário
     form.addEventListener('submit', recebeEventoForm);
