7d846535e1595234ca257b525ec9c8f09f7b2989
Rafael Brigido <rafaelgrohl222@gmail.com>
2020-11-26 12:48:13 -0400

    Curso de JavaScript Moderno do básico ao avançado (ES6)

--- Aula26_Js-atribuicaDesestruturacao_objetos/script.js
+++ Aula26_Js-atribuicaDesestruturacao_objetos/script.js
@@ -1,7 +1,7 @@
 //Atribuição via Desetruturação (Objetos)
 const pessoa = {
     //atributos
-    //nome: 'Luiz',
+    nome: 'Luiz',
     sobrenome: 'Miranda',
     idade: 30,
     //sub-atributos
@@ -14,7 +14,20 @@ const pessoa = {
 //console.log(pessoa.nome);//imprimi atributo nome do objeto pessoa
 
 //Atribuição via Desetruturação
-const {nome = 'não existe', sobrenome, idade} = pessoa;
+//const {nome = 'não existe', sobrenome, idade} = pessoa;
+
+//Imprimi objeto - Pessoa
+const {nome, sobrenome, idade} = pessoa;//objeto pessoa
+
+console.log('Objeto: Pessoa:');
 console.log(pessoa.nome);//resultado: undefined
 console.log(nome, sobrenome, idade);
+
+console.log('-----//-----');//espaço
+
+//Imprimi objeto - endereco
+const {endereco: {rua, numero} } = pessoa;//objeto endereco
+
+console.log('Objeto: Endereço:');
+console.log(rua, numero);
 //4min
