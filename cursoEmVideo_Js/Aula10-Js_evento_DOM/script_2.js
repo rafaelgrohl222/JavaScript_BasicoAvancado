a8ae6e816a8d3958366b1e216403f14c111d3274
Rafael Brigido <rafaelgrohl222@gmail.com>
2021-01-10 18:12:14 -0400

    Curso de JavaScript Moderno do básico ao avançado (ES6)

--- cursoEmVideo_Js/Aula10-Js_evento_DOM/script_2.js
+++ cursoEmVideo_Js/Aula10-Js_evento_DOM/script_2.js
@@ -3,11 +3,19 @@
      var tn2 = document.querySelector('input#txtn2');
      var res = document.getElementById('res');
 
-     var num1 = Number(tn1.value);
-     var num2 = Number(tn2.value);
-     var soma = num1 + num2;
-     res.innerHTML = `A soma entre ${num1} e ${num2} é: <strong>(${soma})</strong>`;
-
+     var numSoma1 = Number(tn1.value);
+     var numSoma2 = Number(tn2.value);
+     var soma = numSoma1 + numSoma2;
+     res.innerHTML = `A soma entre ${numSoma1} e ${numSoma2} é: <strong>(${soma})</strong>`;
  }
 
- //26min
+ function subtrair() {
+     var tn3 = document.getElementById('txtn3');
+     var tn4 = document.querySelector('input#txtn4');
+     var resSubt = document.getElementById('resSubtra');
+
+     var numSubtr1 = Number(tn3.value);
+     var numSubtr2 = Number(tn4.value);
+     var subtrai = numSubtr1 - numSubtr2;
+     resSubt.innerHTML = `A subtração entre ${numSubtr1} e ${numSubtr2} e: <strong>(${subtrai})</strong>`;
+ }
