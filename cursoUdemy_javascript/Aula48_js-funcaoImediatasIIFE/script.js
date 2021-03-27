0144b27f92cb9712bb2c85ae95846692ff0dbefc
rafaelgrohl222 <rafaelgrohl222@gmail.com>
2021-03-27 10:46:39 -0400

    Curso de JavaScript Moderno do básico ao avançado (ES6)

--- cursoUdemy_javascript/Aula48_js-funcaoImediatasIIFE/script.js
+++ cursoUdemy_javascript/Aula48_js-funcaoImediatasIIFE/script.js
@@ -1,7 +1,19 @@
-/** Funções Imediatas / Funções Auto Invocadas imediatamente
- * IIFE - Immediately Invoked Function Expression
+/** IIFE - Immediately Invoked Function Expression
+ * Funções Imediatas / Funções Auto Invocadas imediatamente
  */
 
-(function () {
-    console.log('Oi');
-})();
+(function (idade, peso, altura) {//IIFE -> Protege o escopo gglobal
+
+    const sobrenome = 'Freitas';
+    function criaNome(nome) {
+        //const nome2 = 'Freitas';//pega 2º
+        return nome + ' ' + sobrenome;
+    }
+
+    function falaNome() {
+        console.log(criaNome('Rafael'));
+    }
+    falaNome();
+    console.log(`Idade: ${idade}, Peso: ${peso} e Altura: ${altura}`);
+
+})(40, 100, 1.79);
